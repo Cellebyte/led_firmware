753aed6f1557f6aa049330c625d56ad68a4b27f7
Cellebyte <marcel.fest@live.de>
2021-12-09 02:07:04 +0100

    mock some typings for pylance

---
+++ typing.py
@@ -0,0 +1,27 @@
+typings = False
+Union = None
+Optional = None
+Any = None
+try:
+    import typing
+
+    typings = True
+except ImportError:
+    pass
+
+
+class Mock:
+    pass
+
+
+if typings:
+    Union = typing.Union
+    Optional = typing.Optional
+    Any = typing.Any
+else:
+    Union = Mock
+    Optional = Mock
+    Any = Mock
+
+Union = Union
+Optional = Optional
