e10a21c3fc26e96c3a135068d1e6bc91ad205eaf
Cellebyte <marcel.fest@live.de>
2022-06-12 10:57:08 +0200

    Added DeviceColorPicker from HTML type

--- led-firmware-frontend/src/views/Home.vue
+++ led-firmware-frontend/src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <device-color-picker/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import DeviceColorPicker from '@/components/DeviceColorPicker.vue';
 
 @Component({
   components: {
     HelloWorld,
+    DeviceColorPicker,
   },
 })
 export default class HomeView extends Vue {}
